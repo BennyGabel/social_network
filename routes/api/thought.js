5cd4f86758652115482e4ea6851fdad2a5cfc891
13 Miles <bennygabel@gmail.com>
2022-12-02 13:46:03 -0500

    remove commented lines on api/thought.js

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -11,23 +11,24 @@ const {
     delReaction
 } = require('../../controller/thought');
 
+
 router
   .route('/')
   .get(getAllThoughts)
   .post(addThought);
+
 
 router
   .route('/:id')  
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
-  // Change the <<deleteThought>> route to ONLY require thought id
-  // .delete(deleteThought);     Move to route below, since it will require to pass userid to be removed from User's thoughts
-
+  
 
 router
   .route('/:thoughtId/reaction')
   .post(addReaction)
   .delete(delReaction)
 
+  
 module.exports = router;
